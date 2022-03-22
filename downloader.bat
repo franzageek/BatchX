4a2cdf3d2f9fcee24a5cf1c451afc26137daeafb
FranzaGeek <88248950+franzageek@users.noreply.github.com>
2022-03-22 19:42:02 +0100

    Add files via upload

---
+++ downloader.bat
@@ -0,0 +1,204 @@
+@echo off
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+set /p lnk=<link.txt
+cls
+echo. 
+echo Downloading %plugin%...%  К л                                             К  2
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лл                                            К  4
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллл                                           К  6
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллл                                          К  8
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллл                                         К  10
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллл                                        К  12
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллл                                       К  14
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллл                                      К  16
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллл                                     К  18
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллл                                    К  20
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллл                                   К  22
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo. 
+echo Downloading %plugin%...%  К лллллллллллл                                  К  24
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллл                                 К  26
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллл                                К  28
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллл                               К  30
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллл                              К  32
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллл                             К  34
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллл                            К  36
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллл                           К  38
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллл                          К  40
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллл                         К  42
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллл                        К  44
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллл                       К  46
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллл                      К  48
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллл                      К  50
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллл                     К  52
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллллл                    К  54
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллл                   К  56
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллллллл                  К  58
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллл                 К  64
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллллллллл                К  68
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллллл               К  70
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллллл               К  72
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллллллллллл              К  74
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллллллл             К  76
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллллллллллллл            К  78
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллллллллл           К  80
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллллллллллллллл          К  82
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллллллллллл         К  84
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллллллллллллллллл        К  86
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллллллллллллл       К  88
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллллллллллллллллллл      К  90
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллллллллллллллл     К  92
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллллллллллллллл     К  94
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллллллллллллллллллллл    К  96
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллллллллллллллллл   К  98
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К лллллллллллллллллллллллллллллллллллллллллллл  К  99
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+cls
+echo.
+echo Downloading %plugin%...%  К ллллллллллллллллллллллллллллллллллллллллллллл К  100
+wget %lnk% 2> nul
+del link.txt
+if exist .wget-hsts del .wget-hsts
+echo %plugin%>>installedplugins.txt
+title %windir%\system32\cmd.exe
+ping 127.0.0.1 -n 1 -w 500000000 > nul
+echo Complete!
+pause
+echo.
+exit /b
